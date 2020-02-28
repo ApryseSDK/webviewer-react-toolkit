a65a7e3ebf57e7528a69ed25e3c78592c79bab23
Liam Ross <liamross.code@gmail.com>
2020-02-28 09:57:36 -0800

    feat(ToolButton): added expandProps for expandable menu button
    appearance is also updated to align with new designs

--- src/components/ToolButton/ToolButton.stories.tsx
+++ src/components/ToolButton/ToolButton.stories.tsx
@@ -1,13 +1,25 @@
 import { action } from '@storybook/addon-actions';
-import { boolean, text } from '@storybook/addon-knobs';
+import { boolean, select } from '@storybook/addon-knobs';
 import React from 'react';
+import { Icon } from '../Icon';
 import { ToolButton } from '../ToolButton';
 import readme from './README.md';
 
 export default { title: 'Components/ToolButton', component: ToolButton, parameters: { readme } };
 
 export const Basic = () => (
-  <ToolButton disabled={boolean('disabled', false)} onClick={action('onClick')}>
-    {text('children', '💥')}
+  <ToolButton
+    disabled={boolean('disabled', false)}
+    onClick={action('onClick')}
+    expandProps={
+      boolean('has expandProps?', false)
+        ? {
+            position: select('expandProps.position', ['bottom', 'right'], 'bottom'),
+            onClick: action('expandProps.onClick'),
+          }
+        : undefined
+    }
+  >
+    <Icon icon="RotateRight" />
   </ToolButton>
 );
--- src/components/ToolButton/ToolButton.tsx
+++ src/components/ToolButton/ToolButton.tsx
@@ -1,28 +1,64 @@
 import classnames from 'classnames';
-import React, { ButtonHTMLAttributes, forwardRef } from 'react';
+import React, { ButtonHTMLAttributes, forwardRef, RefAttributes, useMemo } from 'react';
 import { useAccessibleFocus, useOnClick } from '../../hooks';
+import { Remove } from '../../utils';
+import { Icon } from '../Icon';
+import { IconButton } from '../IconButton';
 
-export interface ToolButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface ToolButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  /**
+   * If provided will add an expand button. You can provide any standard button
+   * props, except for children (children will be a drop down arrow
+   * automatically). There is an additional `position` prop so you can set
+   * the position to be `'right'` rather than the default `'bottom'`.
+   */
+  expandProps?: { position?: 'right' | 'bottom' } & Remove<ButtonHTMLAttributes<HTMLButtonElement>, 'children'> &
+    RefAttributes<HTMLButtonElement>;
+}
 
 export const ToolButton = forwardRef<HTMLButtonElement, ToolButtonProps>(
-  ({ className, children, onClick, ...buttonProps }, ref) => {
+  ({ className, expandProps, children, onClick, ...buttonProps }, ref) => {
     const handleOnClick = useOnClick(onClick, { stopPropagation: true });
 
     const isUserTabbing = useAccessibleFocus();
 
-    const toolButtonClass = classnames(
-      'ui__base ui__toolButton',
-      {
+    const positionRight = expandProps?.position === 'right';
+    const expandClass = expandProps?.className;
+    const hasExpandProps = !!expandProps;
+
+    const classes = useMemo(() => {
+      const enabledObj = {
         'ui__toolButton--disabled': buttonProps.disabled,
         'ui__toolButton--tabbing': isUserTabbing,
-      },
-      className,
-    );
+        'ui__toolButton--expanded': hasExpandProps,
+        'ui__toolButton--right': positionRight,
+        'ui__toolButton--bottom': !positionRight,
+      };
+      const wrapper = classnames('ui__base ui__toolButton', enabledObj);
+      const action = classnames('ui__toolButton__action', enabledObj, className);
+      const expand = classnames('ui__toolButton__expand', enabledObj, expandClass);
+      return { wrapper, action, expand };
+    }, [buttonProps.disabled, className, expandClass, hasExpandProps, isUserTabbing, positionRight]);
 
     return (
-      <button {...buttonProps} className={toolButtonClass} onClick={handleOnClick} ref={ref}>
-        <div className="ui__toolButton__internals">{children}</div>
-      </button>
+      <div className={classes.wrapper}>
+        <IconButton
+          disabled={buttonProps.disabled}
+          {...buttonProps}
+          className={classes.action}
+          onClick={handleOnClick}
+          ref={ref}
+        >
+          {children}
+        </IconButton>
+        {expandProps ? (
+          <IconButton disabled={buttonProps.disabled} {...expandProps} className={classes.expand}>
+            <Icon icon="ChevronDown" />
+          </IconButton>
+        ) : (
+          undefined
+        )}
+      </div>
     );
   },
 );
