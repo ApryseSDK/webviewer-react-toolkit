4cf10f5143e83c422f5fe734dbb6787c7b709c20
Liam Ross <liamross.code@gmail.com>
2020-02-28 12:59:32 -0800

    feat(icons): added Hide, Show and Search icons

---
+++ src/icons/Search.tsx
@@ -0,0 +1,9 @@
+import React, { SVGProps } from 'react';
+
+export function Search(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...props}>
+      <path d="M17.69,15.79a8.63,8.63,0,1,0-1.52,1.51l4.42,4.42a.42.42,0,0,0,.57,0l.94-.95a.39.39,0,0,0,0-.56Zm-5.28,1A6.42,6.42,0,1,1,17.19,12,6.43,6.43,0,0,1,12.41,16.8Z" />
+    </svg>
+  );
+}
