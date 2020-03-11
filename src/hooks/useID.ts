a5775a6fd84967c8c974643bbeb892cb8b311924
Liam Ross <liamross.code@gmail.com>
2020-03-11 12:04:21 -0700

    feat(useID): new hook to generate IDs if not given

---
+++ src/hooks/useID.ts
@@ -0,0 +1,11 @@
+import { useMemo } from 'react';
+import { getStringId } from '../utils';
+
+/**
+ * If an ID is not given, will generate and memoize an ID to use for a11y
+ * or any other purpose.
+ * @param id The optional ID provided by props.
+ */
+export function useID(id?: string) {
+  return useMemo(() => (id ? id : getStringId('label')), [id]);
+}
