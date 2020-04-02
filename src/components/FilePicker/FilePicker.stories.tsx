1e99fc974bf84c834085a5ad3424285b7c6afdb2
Liam Ross <liamross.code@gmail.com>
2020-04-02 10:31:10 -0700

    style(FilePicker): trim long file names

--- src/components/FilePicker/FilePicker.stories.tsx
+++ src/components/FilePicker/FilePicker.stories.tsx
@@ -1,34 +1,34 @@
+import { boolean } from '@storybook/addon-knobs';
 import React from 'react';
 import { action } from '../../storybook-helpers/action/action';
 import { FilePicker } from '../FilePicker';
-import { FilePickerItem } from './FilePicker';
 import readme from './README.md';
 
 export default { title: 'Components/FilePicker', component: FilePicker, parameters: { readme } };
 
-const getFiles = (editable = false): FilePickerItem[] => [
-  {
-    key: 0,
-    name: 'file_1.pdf',
-    onRename: editable ? action('onRename file 1') : undefined,
-    onDelete: editable ? action('onDelete file 1') : undefined,
-  },
-  {
-    key: 1,
-    name: 'file_2.pdf',
-    onRename: editable ? action('onRename file 2') : undefined,
-    onDelete: editable ? action('onDelete file 2') : undefined,
-  },
-];
+export const Basic = () => {
+  const onRename = boolean('has onRename', false);
+  const onDelete = boolean('has onDelete', false);
+  const long = boolean('has long name', false);
 
-export const Basic = () => (
-  <div style={{ width: 200 }}>
-    <FilePicker items={getFiles()} />
-  </div>
-);
-
-export const Editable = () => (
-  <div style={{ width: 200 }}>
-    <FilePicker items={getFiles(true)} />
-  </div>
-);
+  return (
+    <div style={{ width: 200 }}>
+      <FilePicker
+        items={[
+          {
+            key: 0,
+            name: `file_1${long ? '_with_long_name_to_show_cut_off' : ''}.pdf`,
+            onRename: onRename ? action('onRename file 1') : undefined,
+            onDelete: onDelete ? action('onDelete file 1') : undefined,
+          },
+          {
+            key: 1,
+            name: `file_2${long ? '_with_long_name_to_show_cut_off' : ''}.pdf`,
+            onRename: onRename ? action('onRename file 2') : undefined,
+            onDelete: onDelete ? action('onDelete file 2') : undefined,
+          },
+        ]}
+      />
+    </div>
+  );
+};
