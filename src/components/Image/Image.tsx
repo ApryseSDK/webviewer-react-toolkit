7db5cf5a4f614583e0593bc8248999baf42b6046
Liam Ross <liamross.code@gmail.com>
2020-04-08 12:30:14 -0700

    feat(Image): simplify image component
    BREAKING CHANGE: no longer accepts classes prop, image and loading placeholder are no longer wrapped by a div

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -10,18 +10,13 @@ export interface ImageProps extends Remove<ImgHTMLAttributes<HTMLImageElement>, 
    */
   src?: FuturableOrLazy<string>;
   /**
-   * Specific classes for all of the internal elements. If you just wish to give
-   * a class to image, you can use `className`.
-   */
-  classes?: { wrapper?: string; image?: string; overlay?: string };
-  /**
    * Render out an element to be shown while src is loading.
    */
   onRenderLoading?(): ReactNode;
 }
 
 export const Image = forwardRef<HTMLImageElement, ImageProps>(
-  ({ src, classes, onRenderLoading, alt, className, ...imgProps }, ref) => {
+  ({ src, onRenderLoading, alt, className, ...imgProps }, ref) => {
     const [source, setSource] = useState<string | undefined>(typeof src === 'string' ? src : undefined);
 
     const getSource = useCallback(async (srcGetter: FuturableOrLazy<string>) => {
@@ -34,24 +29,12 @@ export const Image = forwardRef<HTMLImageElement, ImageProps>(
       getSource(src);
     }, [src, getSource]);
 
-    const wrapperClass = classnames('ui__base ui__image__wrapper', classes?.wrapper);
+    const imageClass = classnames('ui__image', className);
 
-    const imageClass = classnames('ui__image', { 'ui__image--loading': !source }, classes?.image, className);
-
-    const overlayClass = classnames(
-      'ui__image__overlay',
-      {
-        'ui__image__overlay--loading': !source,
-        'ui__image__overlay--hide': !onRenderLoading,
-      },
-      classes?.overlay,
-    );
-
-    return (
-      <div className={wrapperClass}>
-        <img {...imgProps} alt={alt} src={source} className={imageClass} ref={ref} />
-        <div className={overlayClass}>{onRenderLoading?.()}</div>
-      </div>
+    return source ? (
+      <img {...imgProps} alt={alt} src={source} className={imageClass} ref={ref} />
+    ) : (
+      <>{onRenderLoading?.()}</>
     );
   },
 );
