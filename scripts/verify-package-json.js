41dfc0ffff9e8f24643e62db4ba6d253a50bd82d
Logan Bittner <lbittner@pdftron.com>
2020-05-20 12:41:31 -0700

    add script

---
+++ scripts/verify-package-json.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const fs = require('fs');
+
+/**
+ * This script makes sure that the symlink between this repo and
+ * any local repos are destroyed before commiting
+ */
+(async () => {
+  const jsonPath = path.resolve(__dirname, './package.json');
+  const json = JSON.parse(fs.readFileSync(jsonPath) + '');
+  json.main = "./dist/cjs/index.js";
+  fs.writeFileSync(json, JSON.stringify(json, null, 2));
+})()
