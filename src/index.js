fb2c5fba1efe5431ddd6b9919a5ff35d6c853c6a
Yixiao Chen <yixiaochenyc@gmail.com>
2023-03-02 05:22:55 -0800

    remove extra files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var tslib_1 = require('tslib');
-tslib_1.__exportStar(require('./components'), exports);
-tslib_1.__exportStar(require('./data'), exports);
-tslib_1.__exportStar(require('./hooks'), exports);
-tslib_1.__exportStar(require('./icons'), exports);
-tslib_1.__exportStar(require('./utils'), exports);
-//# sourceMappingURL=index.js.map
