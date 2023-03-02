19566c457ad2d6d1ca4b9c826bcb11d052c95faf
Yixiao Chen <yixiaochenyc@gmail.com>
2023-03-02 05:21:56 -0800

    remove accidentally generated jsmaps

--- src/data/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var tslib_1 = require('tslib');
-tslib_1.__exportStar(require('./file'), exports);
-tslib_1.__exportStar(require('./futurable'), exports);
-tslib_1.__exportStar(require('./memoizedPromise'), exports);
-//# sourceMappingURL=index.js.map
--- src/storybook-helpers/theme/font.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.default = {
-  fontBase: 'Lato,Tahoma,sans-serif',
-  fontCode:
-    '"IBM Plex Mono","Fira Code Retina","Fira Code","FiraCode-Retina",monospace',
-};
-//# sourceMappingURL=font.js.map
